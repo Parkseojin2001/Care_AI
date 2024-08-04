47ad880e3cb175819914bb8d227d2a7d067b1001
leesj00 <144108244+leesj00@users.noreply.github.com>
2024-08-04 23:02:06 +0900

    Update urls.py

--- Backend/login/accounts/urls.py
+++ Backend/login/accounts/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
 from accounts import views
+from .views import UserCreateView
 
 urlpatterns = [
+
+    path('api/users/', UserCreateView.as_view(), name='user-create'),
     
     path('login/', auth_views.LoginView.as_view(template_name='회원가입(다트언어)페이지 링크'), name='login'),
     path('logout/', views.logout_view, name='logout'),
