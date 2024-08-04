170a157846d0421188037b4c83f06fb2e7ba183a
leesj00 <144108244+leesj00@users.noreply.github.com>
2024-08-04 22:03:10 +0900

    Add files via upload

---
+++ Backend/login/accounts/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from django.contrib.auth import views as auth_views
+from accounts import views
+
+urlpatterns = [
+    
+    path('login/', auth_views.LoginView.as_view(template_name='회원가입(다트언어)페이지 링크'), name='login'),
+    path('logout/', views.logout_view, name='logout'),
+    path('signup/', views.signup, name='signup'),
+
+    path('google/login/', views.google_login, name='google_login'),
+    path('google/callback/', views.google_callback, name='google_callback'),
+    path('google/login/finish/', views.GoogleLogin.as_view(),
+         name='google_login_todjango'),
+    
+
+]
