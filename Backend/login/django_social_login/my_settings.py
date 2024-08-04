010ebee19e8e274ed5d0e92d1d4fe72fa6f6d9da
leesj00 <144108244+leesj00@users.noreply.github.com>
2024-08-05 00:18:57 +0900

    Add files via upload

---
+++ Backend/login/django_social_login/my_settings.py
@@ -0,0 +1,12 @@
+SECRET_KEY = 'abcdefg123456789@'
+
+DATABASES = {
+    'default' : {
+        'ENGINE': 'django.db.backends.mysql',
+        'USERNAME': 'care',
+        'EMAIL': 'root@gmail.com',
+        'PASSWORD': '1004',
+        'HOST': '127.0.0.1',
+        'PORT': '8000',
+    }
+}
